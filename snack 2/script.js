37ac2aebf017b00569af39f0a327772ca542a6b8
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-01-16 13:54:43 +0100

    inizio esercizio 2

---
+++ snack 2/script.js
@@ -0,0 +1,8 @@
+/**
+ * devo creare un array di squadre di calcio
+ * dove ogni squadra ha: nome, punti fatti, falli subiti.
+ * soltanto il nome deve essere compilato. Punti fatti e falli subiti partono da 0.
+ * Riempire Punti fatti e falli subiti con numero random.
+ * creare un nuovo array con solo nome e falli subiti.
+ * stampo in console
+ *  */ */
