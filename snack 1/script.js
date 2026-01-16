524db30a28cad10e606db458287e842af3db814a
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-01-16 13:18:36 +0100

    creo array di oggetti

--- snack 1/script.js
+++ snack 1/script.js
@@ -1,1 +1,14 @@
-//Snack 1
+/* Snack 1 
+
+devo creare un array di oggetti bici
+ogni bici ha: nome, peso
+devo trovare la bici col peso minore
+stamparla in console
+
+ */
+
+//creo l'array di oggetti:
+
+const biciclette = [ 
+    
+] 
